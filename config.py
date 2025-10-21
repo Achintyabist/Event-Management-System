c00741112af555079ba87e400422f704d13c020c
Kartik Negi <131234751+kartik21777@users.noreply.github.com>
2025-10-21 11:09:26 +0530

    Initial commit
    Contains basic backend and basic frontend code.

---
+++ config.py
@@ -0,0 +1,12 @@
+
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Kartikbruno@1234',
+    'database': 'Event_Manager',
+    'port': 3306
+}
+
+SECRET_KEY = 'vZTvMApVTKMuoWGr2hr2NLI4CL3oKzEiUtTnN5atlhKxQCPIn7pMMCte3uxWCb5_'
+
+
